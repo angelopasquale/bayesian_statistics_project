377b61966d6fb842700ef3870931b09a30c910ac
matteo contini <lombardata@AAAAAAA.local>
2019-12-04 11:22:19 +0100

    First commit

---
+++ code/codeR.R
@@ -0,0 +1,14 @@
+rm ( list = ls() )
+library("mvtnorm")
+
+#### INPUT OF THE DATA ####
+
+#### CONSTRUCTION OF THE COVARIATES ####
+
+#### HYPERPARAMETERS ####
+
+alpha0 = 1 # Mass parameter of the Dirichlet process
+
+#### SOME MATRICES TO BE PRECOMPUTED ####
+
+#### FIT OF THE MODEL ####
