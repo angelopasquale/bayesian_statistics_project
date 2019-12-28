af859bc0cf6e75c62299595d7f5957a353e01178
Angelo Pasquale <angelo.pasquale@mail.polimi.it>
2019-12-28 17:09:06 +0100

    a working clustering on species

--- code/lastcodeR.R
+++ code/lastcodeR.R
@@ -11,10 +11,10 @@ library(gjam)
 #### INPUT OF THE DATA ####
 
 n = 5 # number of sites
-S = 10 # number of species
-k = 4 # number of covariates (no intercept)
+S = 15 # number of species
+n_cov = 4 # number of covariates (no intercept)
 
-f <- gjamSimData(n = n, S = S, Q = k, typeNames = 'CA')
+f <- gjamSimData(n = n, S = S, Q = n_cov, typeNames = 'CA')
 # The object `f` includes elements needed to analyze the simulated data set.  
 # `f$typeNames` is a length-$S$ `character vector`. The `formula` follows 
 # standard R syntax. It does not start with `y ~`, because gjam is multivariate.
@@ -43,13 +43,13 @@ R <- riwish(S + 1, diag(S)) # Prior variance-covariance matrix of the e
 # r = number of latent factors, user chosen
 #eta_h = rep ( 0, times=r )
 
-r = 10
+r = 5
 eta_h <- rinvgamma(r, 1/2, 1/10^4)
 
 Dz <- riwish(2 + r - 1, 4 * 1/eta_h * diag(r)) 
 
 # N_stick = truncation level for stick breaking factors, user chosen
-N_stick = 30 
+N_stick = 10 
 #Z <- rmvnorm ( n = N_stick, mean = rep(0, times = r), sigma = Dz ) 
 
 #mu_Zj <- rep(0, times = r)
@@ -65,9 +65,9 @@ nu = 1
 G = 1
 sigmaeps2 <- 1e4
 
-muBeta = rep ( 0, times=k ) # Prior mean of the beta coefficients
+muBeta = rep ( 0, times=n_cov ) # Prior mean of the beta coefficients
 sigmaBeta = diag ( k ) # Prior variance-covariance matrix of the beta coefficients
-B <- matrix(data = 0, nrow = S, ncol = k) # coefficient matrix
+B <- matrix(data = 0, nrow = S, ncol = n_cov) # coefficient matrix
 
 pl = matrix(0,ncol = N_stick,nrow = n_species)
 
@@ -92,7 +92,7 @@ pl <- matrix(0, nrow = n_species, ncol = N_stick)
 posteriorDraws = 3
 burnInIterations = 0
 
-alpha0 = 1e2 # Mass parameter of the Dirichlet process
+alpha0 = 1e1 # Mass parameter of the Dirichlet process
 
 ###############################################
 #STA FUNZIONE è FATTA NELL'hfunction DA .sampleP <- CHIAMATA IN .getPars <-, HO PROVATO A DARCI UN OCCHIO (LA CHIAMA IN 
@@ -115,29 +115,45 @@ compute_GD_prior <- function(N_stick, alpha0){
 
 norm_vec <- function(x) sqrt(sum(x^2))
 
+xi <- compute_GD_prior(N_stick,alpha0)
+# c <- 2
+# given the vector k = [1,..., 1] of length = S
+p[1] <- xi[1]
+p[2:N_stick-1] <- sapply(2:N_stick, function(j) xi[j] * prod(1 - xi[1:(j-1)]))
+p[N_stick] <- 1 - sum( p[1:N_stick-1] )
+
+k = sample(1:N_stick, size = n_species, replace=TRUE, prob = p)
+
+names(k) <- seq(1,n_species)
+
 for ( niter in 1:(posteriorDraws + burnInIterations) ) { # MCMC loop
+  
   # Step 1 : resample the cluster assignments
   #for each species j = 1,...,n_species do
   Z <- matrix(data = 0, nrow = N_stick, ncol = r) 
   Q <- matrix(data = 0, nrow = n_species, ncol = N_stick) 
   
-  xi <- compute_GD_prior(N_stick,alpha0)
-  # c <- 2
-  # given the vector k = [1,..., 1] of length = S
-  p[1] <- xi[1]
-  p[2:N_stick-1] <- sapply(2:N_stick, function(j) xi[j] * prod(1 - xi[1:(j-1)]))
-  p[N_stick] <- 1 - sum( p[1:N_stick-1] )
+  # Step 3
+  for ( l in 1:n_species ) { # Loop on species
+    for (j in 1:N_stick) {
+      pl[l,j] <- p[j] * exp( -1/(2*sigmaeps2) * norm_vec(V[,l] - x %*% B[l,] - W %*% Z[j,])^2)
+    }
+    k[l] = sample(N_stick, size = 1, replace=TRUE, prob = pl[l,])
+  }
   
+  cardinality_S = rep(0, times = N_stick)
+                      
   for ( j in 1:N_stick ) { 
     # Step 1
       print(j)
     if (!(j %in% k)) {
       print('entered if')
       Z[j,] = rmvnorm ( n = 1, mean = rep(0, times = r), sigma = Dz )
+      cardinality_S[j] = 1
     } 
     else { # if j in k
       print('entered else')
-      cardinality_S[j] = cardinality_S[j] + sum( j == k )
+      cardinality_S[j] = cardinality_S[j] + 1
       print(cardinality_S[j])
       Sigma_Zj = solve(cardinality_S[j]/sigmaeps2 * t(W) %*% W + solve(Dz) )
       mu_Zj = c(0)
@@ -153,13 +169,8 @@ for ( niter in 1:(posteriorDraws + burnInIterations) ) { # MCMC loop
   }
   
   A = Q %*% Z
-  
-  # Step 3
-  for ( l in 1:n_species ) { # Loop on species
-    for (j in 1:N_stick) {
-      pl[l,j] <- p[j] * exp( -1/(2*sigmaeps2) * norm_vec(V[,l] - x %*% B[l,] - W %*% Z[j,])^2)
-    }
-    k[l] = sample(N_stick, size = 1, replace=TRUE, prob = pl[l,])
-  }
-  
+
 } 
+
+print(k)
+print(A)
