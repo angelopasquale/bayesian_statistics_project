afe4e67648cad8f56ad6bcc625c5125c06e02eb0
Angelo Pasquale <angelo.pasquale@mail.polimi.it>
2020-01-07 17:11:01 +0100

    GJAM_Gibbs_Sampler.R function for MCMC and main test.R, now the sampler returns the chains of parameters

---
+++ code/GJAM_Gibbs_Sampler.R
@@ -0,0 +1,227 @@
+GJAM_Gibbs_Sampler <- function(x, Y, r, N_stick, alpha0, posteriorDraws, burnInIterations){
+  
+  # INPUT PARAMETERS
+  n = nrow(Y) # number of sites
+  S = ncol(Y) # number of species
+  n_cov = ncol(x) # number of covariates (no intercept)
+  # r = number of latent factors
+  # N_stick = truncation level for stick breaking factors, user chosen
+  # alpha0 = Mass parameter of the Dirichlet process
+  
+  # OUTPUT PARAMETERS
+  list_B <- list()
+  list_A <- list()
+  list_Z <- list()
+  list_k <- list()
+  list_sigmaeps2 <- list()
+  
+  # 3.A.2 for simulation of latent variable V
+  #V = Y
+  
+  #### CONSTRUCTION OF THE COVARIATES ####
+  
+  #### HYPERPARAMETERS ####
+  
+  #### SOME MATRICES TO BE PRECOMPUTED ####
+  
+  mu_e = rep ( 0, times=S ) # Prior mean of e
+  R <- riwish(S + 1, diag(S)) # Prior variance-covariance matrix of the e 
+  
+  # for inverse wishart and related computations see gjam library (for instance gjamSimData function), 
+  # it is done using Rcpp 
+  
+  # r = number of latent factors, user chosen
+  #eta_h = rep ( 0, times=r )
+  
+  eta_h <- rinvgamma(r, 1/2, 1/10^4)
+  
+  Dz <- riwish(2 + r - 1, 4 * 1/eta_h * diag(r)) 
+  
+  #Z <- rmvnorm ( n = N_stick, mean = rep(0, times = r), sigma = Dz ) 
+  
+  #mu_Zj <- rep(0, times = r)
+  #for (j in seq(1, N)) {
+  #  Z[j,] <- rmvnorm ( n = 1, mean = mu_Zj, sigma = Dz ) 
+  #}
+  
+  W <- rmvnorm ( n = n, mean = rep(0, times = r), sigma = diag(r) ) 
+  # need to transpose for the computation of Bx_i + Q(k)Zw_i'
+  
+  nu = 1
+  G = 1e3
+  sigmaeps2 <- 1e8
+  
+  pl = matrix(0,ncol = N_stick,nrow = S)
+  
+  
+  muBeta = rep ( 0, times=n_cov ) # Prior mean of the beta coefficients
+  sigmaBeta = diag ( n_cov ) # Prior variance-covariance matrix of the beta coefficients
+  B <- matrix(data = 0, nrow = S, ncol = n_cov) # coefficient matrix
+  
+  for (j in seq(1,S,1)) {
+    B[j,] <- rmvnorm ( n = 1, mean = muBeta, sigma = 100*sigmaBeta )
+  }
+  sigmaB = 10
+  
+  cardinality_S = rep ( 0, times=N_stick )
+  
+  # Dirichlet process
+  
+  # each label belongs to {1,...,N_stick}
+  k = c();
+  
+  p <- numeric(N_stick)
+  pl <- matrix(0, nrow = S, ncol = N_stick)
+  
+  xi <- compute_GD_prior(N_stick,alpha0,k)
+  # c <- 2
+  # given the vector k = [1,..., 1] of length = S
+  p[1] <- xi[1]
+  p[2:N_stick-1] <- sapply(2:N_stick, function(j) xi[j] * prod(1 - xi[1:(j-1)]))
+  p[N_stick] <- 1 - sum( p[1:N_stick-1] )
+  
+  k = sample(1:N_stick, size = S, replace=TRUE, prob = p)
+  
+  names(k) <- seq(1,S)
+  
+  V = Y
+  V_star = matrix(0,nrow = nrow(Y),ncol = ncol(Y))
+  
+  # start of the Gibbs sampler
+  for ( niter in 1:(posteriorDraws + burnInIterations) ) { # MCMC loop
+    
+    # Step 1 : resample the cluster assignments
+    #for each species j = 1,...,S do
+    Z <- matrix(data = 0, nrow = N_stick, ncol = r) 
+    Q <- matrix(data = 0, nrow = S, ncol = N_stick) 
+    
+    # Step 3
+    for ( l in 1:S ) { # Loop on species
+      for (j in 1:N_stick) {
+        pl[l,j] <- p[j] * exp( -1/(2*sigmaeps2) * norm_vec(V[,l] - x %*% B[l,] - W %*% Z[j,])^2)
+      }
+      
+      k[l] = sample(N_stick, size = 1, replace=TRUE, prob = pl[l,])
+    }
+    
+    cardinality_S = rep(0, times = N_stick)
+    
+    names(cardinality_S) = seq(1,N_stick)
+    
+    for ( j in 1:N_stick ) { 
+      
+      # Step 1
+      if (!(j %in% k)) {
+        #print('entered if')
+        Z[j,] = rmvnorm ( n = 1, mean = rep(0, times = r), sigma = Dz )
+        cardinality_S[j] = 1
+      } 
+      else { # if j in k
+        #print('entered else')
+        cardinality_S[j] = sum(j==k)
+        #print(paste('cardinality',cardinality_S[j]))
+        Sigma_Zj = solve(cardinality_S[j]/sigmaeps2 * t(W) %*% W + solve(Dz) )
+        mu_Zj = c(0)
+        for (l in 1:S) {
+          if (k[l] == j){
+            #print('entered else-if')
+            mu_Zj = mu_Zj + 1/sigmaeps2 * Sigma_Zj %*% t(W) %*% ( t(t(V[,l])) - x %*% B[l,] )
+          }
+          Q[l,k[l]] = 1
+        }
+        Z[j,] = rmvnorm ( n = 1, mean = mu_Zj, sigma = Sigma_Zj )
+      }
+    }
+    
+    A = Q %*% Z
+    
+    # Step 2
+    for ( i in 1:n ) {
+      Sigma_W = solve(1/sigmaeps2 * t(A) %*% A + diag(r))
+      mu_W = 1/sigmaeps2 * Sigma_W %*% t(A) %*% (t(t(V[i,])) - B %*% x[i,])
+      W[i,] = rmvnorm ( n = 1, mean = mu_W, sigma = Sigma_W )
+    }
+    
+    # Step 5
+    dx = c(0)
+    for (i in 1:n) {
+      dx = dx + norm_vec(V[i,] - B %*% x[i,] - A %*% W[i,])^2
+    }
+    sigmaeps2 = invgamma::rinvgamma(1,shape = (n * S + nu)/2 + 1, rate = dx/2 + nu/G^2)
+    #sigmaeps2 = 1
+    
+    # Step 6
+    Dz = riwish(2 + r + N_stick - 1, t(Z) %*% Z + 4 * 1/eta_h * diag(r))
+    
+    # Step 7
+    Sigma_star = A %*% t(A) + sigmaeps2 * diag(S)
+    D = diag(diag(Sigma_star))
+    R = D^(1/2) %*% Sigma_star %*% solve(D)^(1/2)
+    B_star = D^(1/2) %*% B
+    
+    for (i in seq(1,n)) {
+      for (j in seq(1,S)) {
+        
+        mean1 = as.vector(B_star[j,] %*% x[i,] + A[j,] %*% W[i,])
+        
+        if (Y[i,j] == 1){
+          V_star[i,j] <- .tnorm(1, 0, Inf, mean1, sigmaeps2) 
+        } else {
+          V_star[i,j] <- .tnorm(1, -Inf, 0, mean1, sigmaeps2) 
+        }
+      }
+    }
+    
+    V = V_star %*% solve(D)^(1/2)
+    
+    # Step 8
+    for (j in seq(1,S,1)) {
+      muBetaj = solve(1/sigmaB^2 * diag(n_cov) + 1/sigmaeps2 * t(x) %*% x) %*% t(x) %*% (V_star[,j] - W %*% A[j,]) * 1/sigmaeps2
+      sigmaBetaj = solve(1/(sigmaB^2) * diag(n_cov) + 1/sigmaeps2 * t(x) %*% x)
+      B[j,] <- rmvnorm ( n = 1, mean = muBetaj, sigma = sigmaBetaj )
+    }
+    
+    list_B[[niter]] <- B
+    list_A[[niter]] <- A
+    list_Z[[niter]] <- Z
+    list_k[[niter]] <- k
+    list_sigmaeps2[[niter]] <- sigmaeps2
+  } 
+  # end of Gibbs sampler
+  return(list(B = list_B, A = list_A, Z = list_Z, k = list_k, sigmaeps2 = list_sigmaeps2))
+}
+
+.tnorm <- function(n,lo,hi,mu,sig){   
+  
+  #normal truncated lo and hi
+  
+  tiny <- 10e-6
+  
+  if(length(lo) == 1 & length(mu) > 1)lo <- rep(lo,length(mu))
+  if(length(hi) == 1 & length(mu) > 1)hi <- rep(hi,length(mu))
+  
+  q1 <- pnorm(lo,mu,sig)
+  q2 <- pnorm(hi,mu,sig) 
+  
+  z <- runif(n,q1,q2)
+  z <- qnorm(z,mu,sig)
+  
+  z[z == Inf]  <- lo[z == Inf] + tiny
+  z[z == -Inf] <- hi[z == -Inf] - tiny
+  z
+}
+
+compute_GD_prior <- function(N_stick, alpha0, k){
+  xi <- numeric(N_stick)
+  for (j in seq(1, N_stick-1)) {
+    shape1 = alpha0/N_stick + sum(k == j)
+    shape2 = (N_stick - j)/N_stick * alpha0
+    for (s in seq(j + 1, N_stick)) {
+      shape2 = shape2 + sum(k == s)
+    }
+    xi[j] <- rbeta(1, shape1 = shape1, shape2 = shape2)
+  }
+  return(xi)
+}
+
+norm_vec <- function(x) sqrt(sum(x^2))
---
+++ code/test.R
@@ -0,0 +1,53 @@
+rm ( list = ls() )
+setwd("/Users/angelopasquale/Documents/University/LM/YEAR2/SEM1/BS/Project/Bayesian_Statistics_Project/code")
+library("mvtnorm")
+library("matlib")
+library("devtools")
+#install_github("cran/MCMCpack")#, for inverse wishart, but just for the moment
+library("MCMCpack")
+library("invgamma")
+library("MixMatrix")
+library("gdirmn")
+library(gjam)
+
+n = 15
+S = 20
+n_cov = 3
+
+f <- gjamSimData(n = n, S = S, Q = n_cov, typeNames = 'PA')
+# The object `f` includes elements needed to analyze the simulated data set.  
+# `f$typeNames` is a length-$S$ `character vector`. The `formula` follows 
+# standard R syntax. It does not start with `y ~`, because gjam is multivariate.
+# The multivariate response is supplied as a $n \times S$ `matrix` or
+# `data.frame ydata`.
+summary(f)
+#ml  <- list(ng = 1000, burnin = 100, typeNames = f$typeNames)
+#out <- gjam(f$formula, f$xdata, f$ydata, modelList = ml)
+#summary(out)
+
+x = as.matrix(f$xdata) # matrix of measured (environmental) covariates (n * k)
+Y = as.matrix(f$ydata) # matrix of n_species presence/absence data (in a continuous framework) (n * S)
+
+# Number of iterations
+posteriorDraws = 1e3
+burnInIterations = 1
+
+# Number of latent factors
+r = 5
+N_stick = 20
+alpha_0 = 1e2
+
+# burnInIterations: butto via i valori fino al burnInIterations per mantenere
+# solo la stazionare
+# A, sigmaeps2, B, k
+
+source(file = "GJAM_Gibbs_Sampler.R")
+return_list <- GJAM_Gibbs_Sampler(x, Y, r, N_stick, alpha_0, posteriorDraws, burnInIterations)
+
+# voglio la distribuzione di un nuovo Y -> Y_new sim N(mu_new, sigma_new).
+# dalla catena ho la distribuzione a posteriori di mu_new e di sigma_new.
+# per ogni elemento della lista, per ogni B, mi calcolo mu e sigma (media e varianza
+# a posteriori). Se aggiungo un nuovo sito, avrò un nuovo elemento e voglio la predittiva
+# 
+
+# 1. ANALISI DELLA CONVERGENZA CATENE 
