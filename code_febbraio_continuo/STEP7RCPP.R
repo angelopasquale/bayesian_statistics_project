c6f58e7ef1e79b0cc9cdddd4609547e785a25ef0
lorenzofiorello <58304579+lorenzofiorello@users.noreply.github.com>
2020-02-14 13:10:46 +0100

    Continuous version of the algorithm

---
+++ code_febbraio_continuo/STEP7RCPP.R
@@ -0,0 +1,24 @@
+STEP7RCPP<-function(A,sigmaeps2,S,B,n_sites,W,Y,x){
+  Sigma_star = A %*% t(A) + sigmaeps2 * diag(S)
+  D = diag(diag(Sigma_star))
+  R = solveRcpp(D)^(1/2) %*% Sigma_star %*% solveRcpp(D)^(1/2)
+  B_star = solveRcpp(D)^(1/2) %*% B
+  V_star=matrix(0,nrow=n_sites,ncol=S)
+  
+  for (i in seq(1,n_sites)) {
+    for (j in seq(1,S)) {
+      
+      mean1 = as.vector(B_star[j,] %*% x[i,] + A[j,] %*% W[i,])
+      
+      if (Y[i,j] == 1){
+        V_star[i,j] <- .tnorm(1, 0, Inf, mean1, sigmaeps2)
+      } else {
+        V_star[i,j] <- .tnorm(1, -Inf, 0, mean1, sigmaeps2)
+      }
+    }
+  }
+  
+  V = V_star %*% solveRcpp(D)^(1/2)
+  V
+  
+}
